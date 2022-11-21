66f4e29ef28125f6f2bdd89e9f24260417042e94
Eduardo R <93885600+Ed-rar@users.noreply.github.com>
2022-11-20 23:25:16 -0300

    Add files via upload

---
+++ agiobank/Cadastro.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from "react";
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  Button,
+  ActivityIndicator, Pressable, Image, StyleSheet, BackHandler, Modal,
+} from "react-native";
+
+function TelaDeCadastro({ navigation }) {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ margin: 10 }}>Tela_de_cadastro</Text>
+    </View>
+  )
+};
+
+export default TelaDeCadastro;
